2eb8b580c1035005eac4b558ff8ab055fd5c16b0
Yusra AlSayyad <yusra.alsayyad@gmail.com>
2015-02-17 19:06:01 -0600

    Add blank file for simpleSearch.php

---
+++ simpleSearch.php
@@ -0,0 +1,12 @@
+<?php 
+$page_title = "Search Absorption Systems";
+include 'includes/header.html'; 
+require_once 'includes/funcs.inc';
+?>
+<div class="col_2">
+</div>
+
+<div class="col_10">
+
+</div>
+</div>
